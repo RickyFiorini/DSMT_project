88272abca0200a519c8d3fa20654071f81207397
Rick <studenti@localhost>
2024-02-10 14:00:30 +0100

    Message and Notification Entities

---
+++ tomcat_server/src/main/java/it/unipi/dsmt/app/entities/Message.java
@@ -0,0 +1,51 @@
+package it.unipi.dsmt.app.entities;
+
+import java.sql.Timestamp;
+
+// Class for the message info
+public class Message {
+    private String content;
+    private String sender;
+    private Timestamp creationTime;
+    private int chatID;
+
+    public Message(String content, String sender, Timestamp creationTime, int chatID) {
+        this.content = content;
+        this.sender = sender;
+        this.creationTime = creationTime;
+        this.chatID = chatID;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return this.sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public Timestamp getCreationTime() {
+        return this.creationTime;
+    }
+
+    public void setCreationTime(Timestamp creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public int getChatID() {
+        return this.chatID;
+    }
+
+    public void setChatID(int chatID) {
+        this.chatID = chatID;
+    }
+
+}
