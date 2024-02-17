4b9ffb3f33499df96ffd3208f7ec93fc5006923c
Rick <studenti@localhost>
2024-02-17 18:29:13 +0100

    js and css mockup files

---
+++ tomcat_server/src/main/java/it/unipi/dsmt/app/enums/PokemonType.java
@@ -0,0 +1,22 @@
+package it.unipi.dsmt.app.enums;
+
+public enum PokemonType {
+    Normal,
+    Fire,
+    Water,
+    Electric,
+    Grass,
+    Ice,
+    Fighting,
+    Poison,
+    Ground,
+    Flying,
+    Psychic,
+    Bug,
+    Rock,
+    Ghost,
+    Dragon,
+    Dark,
+    Steel,
+    Fairy
+}
